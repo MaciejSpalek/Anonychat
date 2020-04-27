44463b4d28e9378744e1d58dda1317c4fff7d080
MaciejSpalek <spwrtt@gmail.com>
2020-04-27 12:33:30 +0200

    feat: add routing and create basic containers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import styled from 'styled-components';
+import { ThemeProvider } from 'styled-components';
+import theme from './Theme/theme';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './Containers/Navbar/navbar';
+import Home from './Containers/Home/home';
+import Chat from './Containers/Chat/chat';
 
-const Container = styled.div``
 
 const App = () => {
   return (
-    <Container>
-    
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Router>  
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/chat" component={Chat} />
+        </Switch>
+      </Router> 
+    </ThemeProvider>
   );
 }
 
