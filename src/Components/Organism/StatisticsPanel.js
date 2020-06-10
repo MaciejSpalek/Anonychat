c67b3d4b7e6e9b9d8584ccc9583f41975c8e78b2
MaciejSpalek <spwrtt@gmail.com>
2020-06-10 16:28:37 +0200

    wip: create infoPanel.js

--- src/Components/Organism/StatisticsPanel.js
+++ src/Components/Organism/StatisticsPanel.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import StatsLabel from '../Molecules/statsLabel';
+import InfoLabel from '../Molecules/infoLabel';
+
 import { useSelector } from 'react-redux';
-import { FlexColumn } from '../../Theme/mixins';
+import { FlexColumn, FlexCenter } from '../../Theme/mixins';
 import { faUsers, faDoorClosed, faDoorOpen } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
 
 
 const Container = styled.div`
@@ -11,14 +14,20 @@ const Container = styled.div`
     top: calc(50% + 35px);
     left: 50%;
     ${FlexColumn};
+    justify-content: space-between;
     height: calc(100vh - 90px);
     width: 95vw;
+    font-size: 4rem;
     background-color: ${({theme}) => theme.colors.primaryBlue};
     border-radius: .5rem;
     transform: translate(-50%, -50%);
     transition: .3s cubic-bezier(0.785, 0.135, 0.15, 0.86);
     opacity: ${props => props.status  ? 1 : 0};
     z-index: ${props => props.status  ? 100 : 0};
+`
+
+const StyledStatsWrapper = styled.div`
+    ${FlexColumn};
 `
 
 const StatisticsPanel = () => {
@@ -29,23 +38,29 @@ const StatisticsPanel = () => {
 
     return (
         <Container status={statsPanelStatus}>
-            <StatsLabel 
-                text={"Online users"}
-                variable={users}
-                icon={faUsers}
-                iconSize={30}
-            />
-            <StatsLabel 
-                text={"All rooms"}
-                variable={allRooms}
-                icon={faDoorClosed}
-                iconSize={30}
-            />
-            <StatsLabel 
-                text={"Empty rooms"}
-                variable={emptyRooms}
-                icon={faDoorOpen}
-                iconSize={30}
+            <StyledStatsWrapper>
+                <StatsLabel 
+                    text={"Online users"}
+                    variable={users}
+                    icon={faUsers}
+                    // iconSize={30}
+                />
+                <StatsLabel 
+                    text={"All rooms"}
+                    variable={allRooms}
+                    icon={faDoorClosed}
+                    // iconSize={30}
+                />
+                <StatsLabel 
+                    text={"Empty rooms"}
+                    variable={emptyRooms}
+                    icon={faDoorOpen}
+                    // iconSize={30}
+                />
+            </StyledStatsWrapper>
+            <InfoLabel 
+                icon={faGithub}
+                // iconSize={35}
             />
         </Container>
     )
