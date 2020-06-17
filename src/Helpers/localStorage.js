d1f1ba5841408f9cdd51e5eb60f0cfc0d8542f66
MaciejSpalek <spwrtt@gmail.com>
2020-06-17 11:26:33 +0200

    refactor: delete unecessary files

--- src/Helpers/localStorage.js
+++
@@ -1,9 +0,0 @@
-// users
-export const setStorageItem = (currentItem, newItem) => {
-    localStorage.setItem(currentItem, JSON.stringify(newItem))  ;
-}
-
-export const getStorageItem = (item) => {
-    return JSON.parse(localStorage.getItem(item));
-}
-
