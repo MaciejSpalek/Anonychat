8b02c9a5fd35ce734759fd376f2e81fa6b2960bf
MaciejSpalek <spwrtt@gmail.com>
2020-11-25 12:51:08 +0100

    Install PropTypes

--- src/Components/Atoms/Icon/Icon.styled.js
+++ src/Components/Atoms/Icon/Icon.styled.js
@@ -2,6 +2,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styled from 'styled-components';
 
 export const StyledIcon = styled(FontAwesomeIcon)`
-  font-size: 30px;
+  font-size: 34px;
   color: ${({ theme }) => theme.colors.primaryRed};
 `;
