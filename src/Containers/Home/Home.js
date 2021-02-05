9f9ab8e7e70a453a5a564a511dee1f6076928382
MaciejSpalek <spwrtt@gmail.com>
2021-02-05 17:22:41 +0100

    Fix defect on Home.js

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import Header from 'Components/Molecules/Header';
 import LoadingPanel from 'Components/Organism/LoadingPanel/LoadingPanel';
-import { useLocation } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { leaveTheRoom } from 'helpers/functions'
 import { socket } from 'socketClient/socketClient';
@@ -13,10 +13,10 @@ const Home = () => {
     const currentRoom = useSelector(state => state.rooms.currentRoom);
     const dispatch = useDispatch();
     const location = useLocation();
-    
+
     useEffect(()=> {
         leaveTheRoom(currentRoom, socket, dispatch);
-    }, [location.pathname]);
+    }, [location.pathname, currentRoom]);
     
     return (
       <StyledContainer>
